60882c19faac53caef7b8424ab2e54c8a743928c
oddalot <oddalot@yahoo.com>
2017-05-04 03:20:18 -0400

    first finish of list screen

--- public/javascript/app/controllers/listCtrl.js
+++ public/javascript/app/controllers/listCtrl.js
@@ -1,3 +1,27 @@
-app.controller('ListCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth) {
+app.controller('ListCtrl', ['$scope', '$firebaseAuth', '$firebaseArray', function($scope, $firebaseAuth, $firebaseArray) {
+  var ref = firebase.database().ref().child("listItems");
+  $scope.listItems = $firebaseArray(ref);
 
+  $scope.addNewItem = function () {
+    $scope.listItems.$add({
+      text: '',
+      checked: false
+    }).then(function (ref) {
+      document.getElementById(ref.key).focus();
+    });
+  };
+
+  $scope.deleteItem = function (listItem) {
+    $scope.listItems.$remove(listItem);
+  };
+
+  $scope.checkItem = function (listItem) {
+    listItem.checked = true;
+    $scope.listItems.$save(listItem);
+  };
+
+  $scope.uncheckItem = function (listItem) {
+    listItem.checked = false;
+    $scope.listItems.$save(listItem);
+  };
 }]);
