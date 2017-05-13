fcf21b0e2b2244beb31efda3165d6f15780ba671
oddalot <oddalot@yahoo.com>
2017-05-13 13:00:33 -0400

    change offclick directive

--- public/javascript/app/directives/offClick.js
+++ public/javascript/app/directives/offClick.js
@@ -1,6 +1,15 @@
 app.directive('tdOffClick', ['$document', '$parse', function ($document, $parse) {
   return {
     link: function link(scope, element, attrs) {
+      element.on("click", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        var expression = attrs.tdOnClick;
+        scope.$eval(expression);
+        scope.$apply();
+      });
+
       $document.on("click", function (event) {
         if (event.target != element) {
           var expression = attrs.tdOffClick;
